566d561a6fca94adece345bd21f0d0e9b117de25
Augusto Lima <augustolimads@gmail.com>
2021-05-18 17:24:49 -0300

    add mediaQuery

--- src/components/EmptyList/EmptyList.component.tsx
+++ src/components/EmptyList/EmptyList.component.tsx
@@ -1,17 +1,37 @@
-import React from "react";
-import { Image } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Image, Keyboard } from "react-native";
 import { Spacer } from "../Spacer/Spacer.component";
 import * as S from "./EmptyList.styled";
 import emptyListImg from "../../assets/img/Octocat.png";
+import { mediaQuery } from "../../utils/mediaQuery";
 
 interface EmptyListProps {
   text?: string;
 }
 
 export function EmptyList({ text }: EmptyListProps) {
+  const [keyboardIsShow, setKeyboardIsShow] = useState(false);
+  useEffect(() => {
+    Keyboard.addListener("keyboardWillShow", () => {
+      setKeyboardIsShow(true);
+    });
+    Keyboard.addListener("keyboardDidHide", () => {
+      setKeyboardIsShow(false);
+    });
+
+    () => {
+      Keyboard.removeAllListeners("keyboardDidShow");
+      Keyboard.removeAllListeners("keyboardDidHide");
+    };
+  }, []);
   return (
     <S.EmptyListFeedback>
-      <Image source={emptyListImg} style={{ height: 160, width: 160 }} />
+      {!keyboardIsShow && (
+        <Image
+          source={emptyListImg}
+          style={{ height: mediaQuery(0.4), width: mediaQuery(0.4) }}
+        />
+      )}
       <Spacer vertical size={8} />
       <S.EmptyListText>
         {text ? text : "Está meio vazio por aqui!\nBusque por um usuário"}
